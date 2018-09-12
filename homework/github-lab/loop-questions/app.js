b99db42424519540aba9f36ebb374c4f28c349bd
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-12 03:48:37 -0500

    commit 9 - created loops.js file

---
+++ homework/github-lab/loop-questions/app.js
@@ -0,0 +1,33 @@
+for (let i = 0; i <= 99; i++) {
+	console.log(i);
+}
+
+for (let i = 99; i >= 0; i--) {
+	console.log(i);
+}
+
+//I think this works
+for (let i = 298; i >= 1; i-=3) {
+	console.log(i);
+}
+
+for (let i = 0; i < 98; i+=2 ) {
+	console.log(i);
+}
+// don't know how to do this
+for (let i = 0; i < 98; i % 2 ) {
+	console.log(i);
+}
+
+//6
+for (let i = 49; i <= 72; i++) {
+	console.log(i);
+}
+//7
+for (let i = 81; i <= 26; i--) {
+	console.log(i);
+}
+//8 I think this is right?
+for (let i = 0; i <= 99; i++) {
+	console.log(Math.pow(i, 3));
+}
